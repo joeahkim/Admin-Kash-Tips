e29598b89220ce756ad2185efca291cab534f115
joakim ireri <joeahkim@gmail.com>
2024-10-18 16:15:12 +0300

    Fixed Update Results

--- app/src/main/java/com/joeahkim/admin_kashtips/Tip2.kt
+++ app/src/main/java/com/joeahkim/admin_kashtips/Tip2.kt
@@ -1,10 +1,10 @@
 package com.joeahkim.admin_kashtips
 
 data class Tip2(
-    val scores: String = "",
-    val result: String = "",
     val homeTeam: String = "",
     val date: String = "",
     val awayTeam: String = "",
-    val prediction: String = ""
+    val prediction: String = "",
+    val scores: String = "",
+    val result: String = "",
 )
